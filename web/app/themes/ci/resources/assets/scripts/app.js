e22a7eaea5e5e6b070def3c5dd40a14b4ce17746
aitor <aitor@e451.net>
2021-02-14 11:18:41 +0100

    Banner desktop retractil

--- web/app/themes/ci/resources/assets/scripts/app.js
+++ web/app/themes/ci/resources/assets/scripts/app.js
@@ -6,5 +6,19 @@ import 'bootstrap';
 import anime from 'animejs';
 
 $(document).ready(() => {
-  // console.log('Hello world');
+  let viewportHeight = $(window).height(),
+  viewportWidth = $(window).width(),
+  banner = $('.banner');
+
+  if (viewportWidth >= 768) {
+    window.onscroll = function() {
+      console.log(window.scrollY);
+      if (window.scrollY > 1) {
+        banner.addClass('cerrado');
+      } else {
+        banner.removeClass('cerrado');
+      }
+    };
+  }
+
 });
--- web/app/themes/ci/resources/views/partials/header.blade.php
+++ web/app/themes/ci/resources/views/partials/header.blade.php
@@ -1,18 +1,18 @@
 <header class="banner">
   <div class="container-fluid">
 
-    <div class="row brand align-items-center flex-column flex-lg-row p-3">
+    <div class="row brand align-items-center flex-column flex-md-row">
       <a class="logo mr-sm-4" href="{{ home_url('/') }}">
         @svg('images.weblog')
       </a>
       <a class="nombre mr-sm-4" href="{{ home_url('/') }}">
         {{ $siteName }}
       </a>
-      <div class="description text-center text-sm-left">
+      <div class="description text-center text-md-left">
         {!! $siteDescription !!}
       </div>
 
-      <nav class="nav-primary mt-3 mt-lg-0">
+      <nav class="nav-primary mt-3 mt-md-0">
         @if (has_nav_menu('primary_navigation'))
           {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
         @endif
