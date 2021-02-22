1df7abaae7ab4657697db729472bc85e852cef61
aitor <aitor@e451.net>
2021-02-22 09:20:44 +0100

    secciones

---
+++ web/app/themes/ci/resources/views/category.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+
+  @if (! have_posts())
+    <x-alert type="warning">
+      {!! __('Sorry, no results were found.', 'sage') !!}
+    </x-alert>
+
+    {!! get_search_form(false) !!}
+  @endif
+
+  <div class="contenido-wrap d-flex justify-content-center">
+    <div class="contenido d-flex flex-wrap abierto justify-content-center">
+      <section class="infinite-scroll-container">
+        @while(have_posts()) @php(the_post())
+          @includeFirst(['partials.content-' . get_post_type(), 'partials.content'])
+        @endwhile
+      </section>
+    </div>
+  </div>
+
+  {!! get_the_posts_navigation() !!}
+@endsection
+
+@section('sidebar')
+  @include('partials.sidebar')
+@endsection
