e04e2044ba48283501c297b3dad0cbe5e8eec150
aitor <aitor@e451.net>
2021-03-02 17:19:00 +0100

    sangre

--- web/app/themes/ci/resources/assets/scripts/app.js
+++ web/app/themes/ci/resources/assets/scripts/app.js
@@ -5,8 +5,7 @@ import 'jquery';
 import 'bootstrap';
 import './sidebar.js';
 var InfiniteScroll = require('infinite-scroll');
-
-// import anime from 'animejs';
+import anime from 'animejs';
 
 $(document).ready(() => {
   let viewportHeight = $(window).height(),
@@ -23,73 +22,152 @@ $(document).ready(() => {
     };
   }
 
-});
-
     // infinite-scroll
-    // -----------------------------------------------
+  // -----------------------------------------------
 
-    let buttonCont = $('.button-container');
+  let buttonCont = $('.button-container');
 
 
-    let main = new InfiniteScroll( '.infinite-scroll-container', {
-      path: '.nav-previous a',
-      append: 'article',
-      history: false,
-      hideNav: '.posts-navigation',
-      button: '.view-more-button',
-      status: '.page-load-status',
-      debug: true,
+  let main = new InfiniteScroll( '.infinite-scroll-container', {
+    path: '.nav-previous a',
+    append: 'article',
+    history: false,
+    hideNav: '.posts-navigation',
+    button: '.view-more-button',
+    status: '.page-load-status',
+    debug: true,
+  });
+
+  main.on( 'append', function( body, path, items, response ) {
+    // crearGotas(items);
+  });
+
+
+  // movimiento browniano
+  // -----------------------------------------------
+
+  let brownies = $('.browni');
+
+  function browni() {
+    $('.browniano').on('animationiteration webkitAnimationIteration oanimationiteration MSAnimationIteration', function(e) {
+      let deg = 0;
+      deg = deg + Math.floor(Math.random() * 40 - 20);
+      $(this).css({top: Math.floor(Math.random() * 5 - 3) + 'px'},0);
+      $(this).css({left: Math.floor(Math.random() * 5 -3) + 'px'},0);
+      $(this).css({transform: 'rotate(' + deg + 'deg)'},0);
     });
 
-    // function onPageLoad() {
-    //   if ( main.loadCount == 1 ) {
-    //     main.options.loadOnScroll = false;
-    //     buttonCont.removeClass('d-none');
-    //     buttonCont.addClass('d-flex');
-    //     main.off( 'load', onPageLoad );
-    //   }
-    // }
+  }
 
-    // function onAppend() {
-    //   botonesDesplegar = $('.btn-desplegar');
-    //   botonesDesplegar.click(desplegar);
-    // }
+  brownies.mouseenter(function() {
+    $(this).addClass('browniano');
+    browni();
+  })
 
-    // main.on( 'load', onPageLoad );
-    // main.on( 'append', onAppend );
-
-    // main.on( 'last', function() {
-    //   buttonCont.hide();
-    // });
+  brownies.mouseleave(function() {
+    $(this).removeClass('browniano');
+  })
 
 
-    // hamburger
-    // -----------------------------------------------
+  // gotas
+  // -----------------------------------------------
 
-    // $('.hamburger').click(function() {
-    //   $(this).toggleClass('is-active');
-    // });
+  function htmlToElement(html) {
+    var template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content.firstChild;
+  }
 
-    // movimiento browniano
+  let gota = htmlToElement('<div class="gota"><div class="gota-rastro"><div class="rastro"></div></div><div class="gota-cabeza"></div></div>');
 
-    let brownies = $('.browni');
+  let gotean = document.getElementsByClassName('gotea');
 
-    function browni() {
-      $('.browniano').on('animationiteration webkitAnimationIteration oanimationiteration MSAnimationIteration', function(e) {
-        let deg = 0;
-        deg = deg + Math.floor(Math.random() * 40 - 20);
-        $(this).css({top: Math.floor(Math.random() * 5 - 3) + 'px'},0);
-        $(this).css({left: Math.floor(Math.random() * 5 -3) + 'px'},0);
-        $(this).css({transform: 'rotate(' + deg + 'deg)'},0);
-      });
+  // esto creaba todas las gotas a la vez
+  // function crearGotas(nodeList) {
+  //   for (let i = 0; i < nodeList.length; i++) {
 
+  //     let numGotas = Math.floor(Math.random() * 5);
+
+  //     for (let index = 0; index < numGotas; index++) {
+  //       nodeList[i].appendChild(gota.cloneNode(true));
+  //     }
+
+  //     let estasGotas = nodeList[i].getElementsByClassName('gota');
+
+  //     for (let g = 0; g < estasGotas.length; g++) {
+  //       let posX = Math.random() * 90 + '%';
+  //       let laGota = estasGotas[g];
+  //       laGota.style.left = posX;
+  //       let elRastro = laGota.getElementsByClassName('gota-rastro');
+  //       let long = Math.random() * 100 + 'px';
+  //       let dur = Math.random() * 5000 + 1000;
+  //       anime({
+  //         targets: elRastro,
+  //         height: long,
+  //         easing: 'easeInOutQuad',
+  //         duration: dur,
+  //       });
+
+  //     }
+  //   }
+  // }
+  // crearGotas(gotean);
+
+
+
+  function crearGotas(node) {
+      let numGotas = Math.floor(Math.random() * 5);
+
+      for (let index = 0; index < numGotas; index++) {
+        node.appendChild(gota.cloneNode(true));
+        console.log(node);
+      }
+
+      let gotas = node.getElementsByClassName('gota');
+
+      for (let g = 0; g < gotas.length; g++) {
+        let posX = Math.random() * 90 + '%';
+        let laGota = gotas[g];
+        laGota.style.left = posX;
+        let elRastro = laGota.getElementsByClassName('gota-rastro');
+        let long = Math.random() * 100 + 'px';
+        let dur = Math.random() * 5000 + 1000;
+        anime({
+          targets: elRastro,
+          height: long,
+          easing: 'easeInOutQuad',
+          duration: dur,
+        });
+
+      }
+      node.setAttribute("class", `goteando ${node.classList}`)
+  }
+
+  crearGotas(gotean[0]);
+
+  // comprobar si está en viewport
+  // https://www.javascripttutorial.net/dom/css/check-if-an-element-is-visible-in-the-viewport/
+
+  function isInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 && rect.top <= (window.innerHeight || document.documentElement.clientHeight)
+      )
+  }
+
+  document.addEventListener('scroll', function () {
+
+    for (let i = 0; i < gotean.length; i++) {
+
+      let gotea = gotean[i];
+
+      if (isInViewport(gotea) && !gotea.classList.contains('goteando')) {
+        crearGotas(gotea);
+        // gotea.setAttribute("class", `goteando ${gotea.classList}`);
+      }
     }
+  });
 
-    brownies.mouseenter(function() {
-      $(this).addClass('browniano');
-      browni();
-    })
 
-    brownies.mouseleave(function() {
-      $(this).removeClass('browniano');
-    })
+});
+
--- web/app/themes/ci/resources/views/partials/content.blade.php
+++ web/app/themes/ci/resources/views/partials/content.blade.php
@@ -1,4 +1,4 @@
-<article @php(post_class())>
+<article @php(post_class('gotea'))>
   <header>
     <h2 class="entry-title">
       <a href="{{ get_permalink() }}">
